37c33f7804e74020c90d0a3263f91c344289736f
Ismoil Hamidzoda <ismoil2888@gmail.com>
2024-03-17 13:28:28 +0500

    теперь можно добавлять публикации

---
+++ publikacii.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const publishedImages = JSON.parse(localStorage.getItem('publishedImages')) || [];
+
+    const postContainer = document.getElementById('postContainer');
+
+    publishedImages.forEach((imageData, index) => {
+        const postElement = createPostElement(imageData, index);
+        postContainer.appendChild(postElement);
+    });
+});
+
+function createPostElement(imageData, index) {
+    const postElement = document.createElement('div');
+    postElement.classList.add('post');
+
+    const imgElement = document.createElement('img');
+    imgElement.src = imageData;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.onclick = function () {
+        deletePost(postElement, index);
+    };
+
+    // Добавляем обработчик события для долгого нажатия
+    let timeoutId;
+    postElement.addEventListener('mousedown', function () {
+        timeoutId = setTimeout(function () {
+            deleteBtn.style.display = 'block';
+        }, 1000); // Устанавливаем время задержки в 1 секунду
+    });
+
+    postElement.addEventListener('mouseup', function () {
+        clearTimeout(timeoutId);
+    });
+
+    const postNavigation = document.createElement('div');
+    postNavigation.classList.add('post-navigation');
+    postNavigation.innerHTML = `
+        <div class="like"><ion-icon class="heart" name="heart"></ion-icon></div>
+        <div class="coment"><ion-icon name="chatbubble"></ion-icon></div>
+        <div class="save"><ion-icon name="bookmark"></ion-icon></div>
+    `;
+
+    postElement.appendChild(imgElement);
+    postElement.appendChild(postNavigation);
+    postElement.appendChild(deleteBtn);
+
+    return postElement;
+}
+
+function deletePost(postElement, index) {
+    postElement.remove();
+
+    const publishedImages = JSON.parse(localStorage.getItem('publishedImages')) || [];
+    publishedImages.splice(index, 1);
+    localStorage.setItem('publishedImages', JSON.stringify(publishedImages));
+}
